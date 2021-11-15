f1493e00d2d68d505af77ddb4cb45d63ee8763a9
Totor <git.demo@gmail.com>
2021-11-15 15:33:41 +0100

    Fonctions créées

--- Tools.ml
+++ Tools.ml
@@ -1,5 +1,6 @@
 open Graph
 
-let clone_nodes gr = assert false
+let clone_nodes gr = n_fold gr new_node empty_graph
 let gmap gr f = assert false
-let add_arc gr id1 id2 len = assert false
+let add_arc gr id1 id2 len = new_arc gr id1 id2 len
+
--- _build/ftest.ml
+++ _build/ftest.ml
@@ -1,4 +1,5 @@
 open Gfile
+open Tools
 
 let () =
 
@@ -28,8 +29,10 @@ let () =
   (* Open file *)
   let graph = from_file infile in
 
+  let graph1 = (clone_nodes graph) in
+  let graph2 = (add_arc graph1 1 2 6) in
   (* Rewrite the graph that has been read. *)
-  let () = write_file outfile graph in
+  let () = write_file outfile graph2 in
 
     ()
 
