fec4ca29143481be5b8b3a1b96802bfc5b30a91c
Le Nezet Kilian <lenezet@insa-20471>
2021-11-17 11:04:15 +0100

    lol

--- Tools.ml
+++ Tools.ml
@@ -2,6 +2,6 @@ open Graph
 
 let clone_nodes gr = n_fold gr new_node empty_graph
  
-let gmap gr f = e_fold gr (fun ngr id1 id2 lbl -> new_arc ngr id1 id2 lbl) (clone_nodes gr)
+let gmap gr f = e_fold gr (fun ngr id1 id2 lbl -> new_arc ngr id1 id2 (f lbl)) (clone_nodes gr)
 
 let add_arc gr id1 id2 len = assert false
