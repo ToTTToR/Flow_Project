16b1d6aa133275c43715385b6cdcca2fecf6bc30
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2021-11-22 22:32:37 +0100

    Algo ford fulkerson marche

--- Tools.ml
+++ Tools.ml
@@ -14,39 +14,47 @@ type path = id list
 (*Create a path between to nodes, return None if no path available*)
 let find_path gr forbidden id1 id2 =  
   let rec create_path forbid = function
-    | [] -> failwith "No valid path"
-    | (id,len)::rest -> if id=id2 then [id] else if (List.exists (fun x -> x=id) forbid) || len=0 then create_path forbid rest
+  (*If node has no child, or if all child has already been visited, not a valid path to make*)
+    | [] -> failwith "No valid path" 
+  (*Check if next node has already been visited, or if out_arc to it is null.*)
+    | (id,len)::rest -> if (List.exists (fun x -> x=id) forbid) || len=0 then create_path forbid rest 
+    else if id=id2 then [id] (*Destination found!*)
     else try 
       id::(create_path (id::forbid) (out_arcs gr id))
-    with e -> create_path forbid rest
+    with e -> create_path forbid rest (*Make another path if previous not valid*)
   in
   try Some (id1 :: (create_path (id1::forbidden) (out_arcs gr id1)))
   with e -> None
 
+(*Find minimum flow value of given path to increment*)
 let rec find_min_flow gr m = function
   | [] -> m
   | [_] -> m
   | id1::id2::rest -> match find_arc gr id1 id2 with
     | None -> failwith "arc non existante"
-    | Some len -> find_min_flow gr (min m len) (id2::rest)  
+    | Some len -> if len=0 then failwith "arc pas possible a traverser" (*This error should not occur though...*)
+    else find_min_flow gr (min m len) (id2::rest)  
 
+(*Make "graphe d'ecart" after given path has been found*)
 let graphe_ecart gr path =
-  let min_flow = find_min_flow gr 9999 path in 
+  let min_flow = find_min_flow gr 9999 path in (*Should initially compare to infinity*)
   let rec helper gr = function
     | [] -> gr
-    | [_] -> printf "Je suis appelé 2\n";gr
-    | id1::id2::rest -> printf "Je suis appelé 1\n";let gr1=(add_arc gr id1 id2 (-min_flow)) in
+    | [_] -> gr
+    (*For every arc pointing forward to destination, decrease flow*)
+    | id1::id2::rest -> let gr1=(add_arc gr id1 id2 (-min_flow)) in
+    (*For every arc pointing forward to origin, increase flow*)
     let gr2=(add_arc gr1 id2 id1 min_flow) in helper gr2 (id2::rest)
   in
   helper gr path 
 
-let print_path path = List.iter (printf "%d ") path;printf("\n")
+let print_path path = (printf "Chemin associé : ");List.iter (printf "%d ") path;printf("\n")
 
 let make_real_path = function
   | None -> []
   | Some x -> x
 
-let test_1 gr id1 id2 =
+(*let test_1 gr id1 id2 =
   let gr1=gmap gr int_of_string in
   let pat1=make_real_path (find_path gr1 [] id1 id2) in
   let gr2=graphe_ecart gr1 pat1 in
@@ -54,12 +62,13 @@ let test_1 gr id1 id2 =
   let gr3=gmap (graphe_ecart gr2 pat2) string_of_int in
   print_path pat1;
   print_path pat2;
-  gr3
+  gr3*)
 
 let ford_fulkerson gr id1 id2=
-  let gr1=gmap gr int_of_string in (*Initialisation*)
+  let gr1=gmap gr int_of_string in
   let rec helper gra = (*Tant qu'on trouve un chemin, on fait le graphe d'écart associé*)
     match (find_path gra [] id1 id2) with 
-      | None -> gra
+      | None -> gra (*Si pas de chemin trouvé, renvoie notre dernier graphe d'écart*)
       | Some pat -> (print_path pat);helper (graphe_ecart gra pat)
-  in gmap (helper gr1) string_of_int
+  in 
+  gmap (helper gr1) string_of_int
--- Tools.mli
+++ Tools.mli
@@ -9,5 +9,3 @@ type path = id list
 val find_path: int graph -> id list -> id -> id -> path option
 val graphe_ecart: int graph -> path -> int graph
 val ford_fulkerson: string graph -> int -> int -> string graph
-
-val test_1: string graph -> int -> int -> string graph
